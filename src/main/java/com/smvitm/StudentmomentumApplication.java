45b3565eff26b25b19f809fd30cb822127b5c845
Mueral <mueralfernandes@gmail.com>
2017-04-15 18:19:57 +0530

    Initial Commit

---
+++ src/main/java/com/smvitm/StudentmomentumApplication.java
@@ -0,0 +1,12 @@
+package com.smvitm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentmomentumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentmomentumApplication.class, args);
+	}
+}
